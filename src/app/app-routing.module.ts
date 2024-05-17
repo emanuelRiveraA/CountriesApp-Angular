b3183d18942eae72aeefce9c91ce575a0865e440
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2024-05-16 22:45:07 -0600

    Configuración de las paginas con sus rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { AboutPageComponent } from './shared/pages/about-page/about-page.compone
 
 const routes: Routes = [
     {
-        path: 'home',
+        path: '',
         component: HomePageComponent
     },
     {
