3b6b962c1a2193d928371c53b316a3dd6466ecc4
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2024-05-21 21:59:14 -0600

    Se crea componente para tabla de paises

--- src/app/countries/coutries.module.ts
+++ src/app/countries/coutries.module.ts
@@ -7,6 +7,7 @@ import { CountryPageComponent } from './pages/country-page/country-page.componen
 import { CountriesRoutingModule } from './countries-routing.module';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
 import { SharedModule } from '../shared/shared.module';
+import { CountryTableComponent } from './components/country-table/country-table.component';
 
 
 
@@ -15,7 +16,8 @@ import { SharedModule } from '../shared/shared.module';
     ByCountryPageComponent,
     ByRegionPageComponent,
     CountryPageComponent,
-    ByCapitalPageComponent
+    ByCapitalPageComponent,
+    CountryTableComponent
   ],
   imports: [
     CommonModule,
