a639a60de15a5a5ad2bb6a805f552de9f0a5e6fd
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2024-08-07 22:37:28 -0600

    Refactorización de codigo

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -10,6 +11,8 @@ import { switchMap } from 'rxjs';
   ]
 })
 export class CountryPageComponent implements OnInit{
+
+  country?: Country;
 
   constructor( 
     private activatedRoute: ActivatedRoute,
@@ -29,8 +32,7 @@ export class CountryPageComponent implements OnInit{
         }
 
         // console.log({ country });
-        console.log('TENEMOS UN PAÍS');
-        return;
+         return this.country = country;
       });
   }
 
