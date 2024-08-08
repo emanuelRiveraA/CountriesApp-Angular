be4e6f7f43c0ddd1d219901db7dbaec1b923a962
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2024-08-07 22:04:45 -0600

    Metodo onInit para reedirigir si no existe el pais

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
@@ -6,6 +9,30 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit{
 
+  constructor( 
+    private activatedRoute: ActivatedRoute,
+    private countriesService: CountriesService,
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap( ({ id }) => this.countriesService.searchCountryByAlphaCode( id ))
+      )
+      .subscribe( country => {
+
+        if(!country){
+          return this.router.navigateByUrl('');
+        }
+
+        // console.log({ country });
+        console.log('TENEMOS UN PAÍS');
+        return;
+      });
+  }
+
+  
 }
