78e806a6d4088576bea4fd1d168ac69b8dec43f1
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2024-08-13 23:07:52 -0600

    Se crea el objeto que guardara el cache

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -3,11 +3,18 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, catchError, delay, map, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 import { Region } from '../interfaces/region';
+import { CacheStore } from '../interfaces/cache-store.interface';
 
 @Injectable({providedIn: 'root'})
 export class CountriesService {
 
     private apiUrl: string = 'https://restcountries.com/v3.1'; 
+
+    public cacheStore: CacheStore = {
+        byCapital:   { term: '', countries: []},
+        byCountries: { term: '', countries: []},
+        byRegion:    { region: '', countries: []},
+    }
 
     constructor(private http: HttpClient) { }
 
