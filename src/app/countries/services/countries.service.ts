14952ad46203e731779bd3d436a6b2b973a169b1
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2024-08-15 22:35:40 -0600

    Se crean metodos para guardar y recuperar información del LocalStorage

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, catchError, delay, map, of, tap } from 'rxjs';
 import { Country } from '../interfaces/country';
-import { Region } from '../interfaces/region';
 import { CacheStore } from '../interfaces/cache-store.interface';
+import { Region } from '../interfaces/region.type';
 
 @Injectable({providedIn: 'root'})
 export class CountriesService {
@@ -13,10 +13,22 @@ export class CountriesService {
     public cacheStore: CacheStore = {
         byCapital:   { term: '', countries: []},
         byCountries: { term: '', countries: []},
-        byRegion:    { region: '', countries: []},
+        byRegion:    { region: ''},
     }
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) {
+        this.loadFromLocalStorage();
+     }
+
+    private saveToLocalStorage() {
+        localStorage.setItem( 'cacheStore', JSON.stringify( this.cacheStore ) );
+    }
+
+    private loadFromLocalStorage(){
+        if ( !localStorage.getItem( 'cacheStore' )) return;
+
+        this.cacheStore = JSON.parse( localStorage.getItem('cacheStore')! );//El operador ! en TypeScript se utiliza para indicar que el valor no será null o undefined. Esto es útil para evitar errores de tipo, pero debe usarse con precaución, ya que si el valor realmente es null, se producirá un error al intentar parsear.
+    }
 
     private getCountriesRequest( url: string ): Observable<Country[]> {
         return this.http.get<Country[]>( url )
@@ -41,7 +53,8 @@ export class CountriesService {
         const url = `${ this.apiUrl }/capital/${ term }`;
         return this.getCountriesRequest( url )
             .pipe(
-                tap( countries => this.cacheStore.byCapital = { term: term, countries: countries } )//se hace esto sin alterar el flujo
+                tap( countries => this.cacheStore.byCapital = { term: term, countries: countries } ),//se hace esto sin alterar el flujo
+                tap( () => this.saveToLocalStorage() ),
             );//disparar operadores de rxjs
         //return this.http.get<Country[]>( url )
             //.pipe(
@@ -53,15 +66,17 @@ export class CountriesService {
         const url = `${ this.apiUrl }/name/${ term }`;
         return this.getCountriesRequest( url )
             .pipe(
-                tap( countries => this.cacheStore.byCountries = { term: term, countries: countries})
+                tap( countries => this.cacheStore.byCountries = { term: term, countries: countries}),
+                tap( () => this.saveToLocalStorage() ),
             );
     }
 
-    searchRegion( region: string): Observable<Region[]>{
+    searchRegion( region: Region): Observable<Region[]>{
         const url = `${ this.apiUrl }/region/${ region }`;
         return this.http.get<Region[]>( url )
             .pipe( 
-                // tap( countries => this.cacheStore.byRegion = { region: region}),
+                tap( countries => this.cacheStore.byRegion = { region: region}),
+                tap( () => this.saveToLocalStorage() ),
                 catchError( () => of([]) )
             );
     }
